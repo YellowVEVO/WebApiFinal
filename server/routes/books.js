e5fbdb10b21b7ce7139f6ecd0acdbf7bdc554054
YellowVEVO <ahmadm708@gmail.com>
2025-04-24 22:27:11 -0600

    fixed books

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Book = require('../models/Book');
+const Book = require('../models/Books');
 const verifyToken = require('../utils/verifyToken');
 
 const router = express.Router();
