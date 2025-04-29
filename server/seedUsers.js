b3924e9c1ff974dd416372cf4b9c1ed6199795ad
YellowVEVO <ahmadm708@gmail.com>
2025-04-29 00:36:13 -0600

    fixed users route

--- server/seedUsers.js
+++ server/seedUsers.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const User = require('./models/user');
+const User = require('./models/User'); // Ensure the correct path and capitalization
 require('dotenv').config({ path: './server/.env' });
 
 const users = [
@@ -10,9 +10,10 @@ const users = [
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(async () => {
-    await User.deleteMany();
+    console.log('Connected to MongoDB');
+    await User.deleteMany(); // Clear existing users
     for (const user of users) {
-      const newUser = new User(user);
+      const newUser = new User(user); // Passwords will be hashed automatically
       await newUser.save();
     }
     console.log('Users seeded');
