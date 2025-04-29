3f27c791ce872853f87067ad5e2600f3f8d50d4f
YellowVEVO <ahmadm708@gmail.com>
2025-04-29 00:43:59 -0600

    wrong fix

--- server/seedBooks.js
+++ server/seedBooks.js
@@ -1,6 +1,6 @@
 require('dotenv').config({ path: './server/.env' });
 const mongoose = require('mongoose');
-const Book = require('./models/Book'); // Correct file name
+const Book = require('./models/Books'); // Correct file name
 
 const books = [
   { title: "1984", author: "George Orwell", genre: "Dystopian", available: true },
